d0558042fd871b395fee7eddb5c5ff3843c65f75
Juri <juri@juris-mbp.tamedia.ch>
2016-04-05 11:01:21 +0200

    now checking if cross domain, also works in Safari

--- iframe_autofit_receive.js
+++ iframe_autofit_receive.js
@@ -1,16 +1,19 @@
 (function() {
-
-
     var onIframeContentResize = function (e) {
 
         var autofitIframes = document.querySelectorAll('iframe.autofit'),
             messageObject = e.data,
-            nuHeight;
+            nuHeight,
+            iframesrc;
 
         if(messageObject.type === 'autofit') {
-
             for(var i = 0; i < autofitIframes.length; i++) {
-                if(autofitIframes[i].src === messageObject.src) {
+                if(domainCheck(autofitIframes[i]) === true) {
+                    iframesrc = autofitIframes[i].contentWindow.location.href;
+                } else {
+                    iframesrc = autofitIframes[i].src;
+                }
+                if(iframesrc === messageObject.src) {
                     nuHeight = messageObject.contentHeight;
                     if(nuHeight !== autofitIframes[i].getAttribute('height')) {
                         autofitIframes[i].setAttribute('height', nuHeight);
@@ -20,7 +23,16 @@
         }
     };
     window.addEventListener('message', onIframeContentResize);
-
 }());
 
-
+// check if the src of iframe is crossdomain
+function domainCheck(iframe) {
+    var html = null;
+    try {
+        var tempdoc = iframe.contentDocument || iframe.contentWindow.document;
+        html = tempdoc.body.innerHTML;
+    } catch(err) {
+        //do nothing on error
+    }
+    return(html !== null);
+}
